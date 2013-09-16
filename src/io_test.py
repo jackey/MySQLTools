d0c761ab916478dc7550f3abf625a58889fd8a8a
jackey <jziwenchen@gmail.com>
2013-09-16 23:58:14 +0800

    desc table

--- src/io_test.py
+++ src/io_test.py
@@ -2,12 +2,30 @@
 
 import os, sys
 import db
-import _mysql
+import MySQLdb
 
 APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(APP_PATH, "src"))
 
-
-
 if __name__ == "__main__":
 	config = db.load_config()
+	try:
+		db = MySQLdb.connect(host="localhost", user="root", passwd="admin", db="test_io")
+	except MySQLdb.Error as e:
+		print "Error: %d: %s" %(e.args[0], e.args[1])
+		sys.exit(1)
+
+	cursor = db.cursor()
+
+	cursor.execute("""
+		desc node
+		""")
+
+	while 1:
+		row = cursor.fetchone()
+		if not row:
+			break
+		print "%s : %s" %(row[0], row[1])
+
+	cursor.close()
+	db.close()
